3d1f304e0e250d6fadc4fdc168c04a2a0f5c3f41
Giuseppe Vetri <giuseppe@codingpizza.com>
2021-10-21 23:08:30 +0200

    Updated dependency and database url

--- shared/src/jvmMain/kotlin/com/codingpizza/financialtracker/di/PlatformModule.kt
+++ shared/src/jvmMain/kotlin/com/codingpizza/financialtracker/di/PlatformModule.kt
@@ -4,15 +4,17 @@ import com.codingpizza.financialtracker.datasource.CacheReceiptDataSource
 import com.codingpizza.financialtracker.datasource.ReceiptLocalDataSource
 import com.codingpizza.financialtracker.db.FinancialTrackerDatabase
 import com.codingpizza.financialtracker.model.DatabaseConfigWrapper
-import com.codingpizza.financialtracker.repositories.ReceiptDtoStoreRepository
-import com.codingpizza.financialtracker.repositories.ReceiptRepository
-import com.codingpizza.financialtracker.repositories.server.ServerReceiptRepository
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.asJdbcDriver
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.koin.core.module.Module
 import org.koin.dsl.module
+
+private const val DB_NAME = "FinancialTracker"
+private const val DB_USER = "financialtracker"
+private const val DB_PASSWORD = "ktorpassword"
+private const val CLOUD_SQL_CONNECTION_NAME = "codingpizza-financialtracker:europe-central2:mysql-instance"
 
 actual fun platformModule(): Module = module {
     single { provideHikariDatasource(databaseConfig = get()) }
@@ -23,18 +25,20 @@ actual fun platformModule(): Module = module {
 
 private fun provideSqlDelightDriver(hikariDataSource: HikariDataSource): SqlDriver {
     val driver: SqlDriver = hikariDataSource.asJdbcDriver()
-    FinancialTrackerDatabase.Schema.migrate(driver,0,1)
+    FinancialTrackerDatabase.Schema.migrate(driver, 0, 1)
     return driver
 }
 
 private fun provideSqlDelightDatabase(driver: SqlDriver): FinancialTrackerDatabase =
     FinancialTrackerDatabase(driver)
 
-private fun provideHikariDatasource(databaseConfig: DatabaseConfigWrapper) : HikariDataSource{
-    val config = HikariConfig().apply {
-        jdbcUrl = databaseConfig.connection
-        username = databaseConfig.username
-        password = databaseConfig.password
-    }
+private fun provideHikariDatasource(databaseConfig: DatabaseConfigWrapper): HikariDataSource {
+    val config = HikariConfig()
+    val connectionUrl = "jdbc:mysql://google/$DB_NAME?cloudSqlInstance=$CLOUD_SQL_CONNECTION_NAME&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=$DB_USER&password=$DB_PASSWORD"
+    config.jdbcUrl = connectionUrl
+    println("Jdbc url: $connectionUrl")
+    config.username = DB_USER
+    config.password = DB_PASSWORD
+
     return HikariDataSource(config)
 }
